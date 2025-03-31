454ace1cfc1f71d32622202d8b944b4f352bb82a
monuImarsar <monu.rajput@imarsar.com>
2025-03-31 13:35:59 +0530

    31-03-25 code pushed

--- src/components/pages/adminctrlmaster/Dashboard/Components/PackagesSpotlight.jsx
+++ src/components/pages/adminctrlmaster/Dashboard/Components/PackagesSpotlight.jsx
@@ -1,71 +1,101 @@
-
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 import useImageFetcher from "../../../../../Custom Components/useImageFetcher";
-import { adjustStringLength, ViewOrDownloandPDF } from "../../../../../service/RedendentData";
-export default function PackagesSpotlight({ packages }) {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const filteredArr = packages?.filter((item) => item?.isActive === 1);
-    setData(filteredArr);
-  }, [packages]);
+import {
+  adjustStringLength,
+  ViewOrDownloandPDF,
+} from "../../../../../service/RedendentData";
+export default function PackagesSpotlight({ packages, title }) {
   return (
-    <div className="p-4 pt-0">
-      <h2 className="text-xl font-semibold mb-0 text-[20px]">Packages in Spotlight</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {data.map((pkg, index) => (
-          <Card index={index} pkg={pkg} />
-        ))}
-      </div>
-    </div>
+    <>
+      {packages?.length > 0 ? (
+        <div className="p-4 pt-0">
+          <h2 className="text-xl font-semibold mb-0 text-[20px]">{title}</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {packages.map((pkg, index) => (
+              <Card index={index} pkg={pkg} title={title} />
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div className="p-4 pt-3">
+          {/* <div className="w-1/3 h-6 bg-gray-300 rounded mb-4 animate-pulse" /> */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {[...Array(12)].map((_, index) => (
+              <SkeletonCard key={index} />
+            ))}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
-const Card = ({ index, pkg }) => {
-  const { imageSrc, loading, error, fetchImage } = useImageFetcher();
-
-  useEffect(() => {
-    const handleShowImage = async () => {
-      await fetchImage(
-        `/tnx_InvestigationAttchment/ViewDocument?Documentpath=${pkg?.image}`
-      );
-    };
-    handleShowImage();
-  }, []);
-
+const Card = ({ index, pkg, title }) => {
   const str = adjustStringLength(pkg?.description, 711);
 
   return (
-    <div key={index} className="bg-white shadow-md rounded-lg overflow-hidden">
-      {/* Fixed image container */}
-      <div className="w-full h-40 flex justify-center items-center bg-white-200">
-        <img
-          src={imageSrc}
-          alt={pkg.subject}
-          className="w-full h-full object-contain"
-        />
-      </div>
-      <div className="p-4">
-        <h3 className="text-sm font-bold">Here's Our {pkg.subject}</h3>
-        <button
-          onClick={() => {
-            ViewOrDownloandPDF(
-              `/tnx_InvestigationAttchment/ViewDocument?Documentpath=${pkg?.pdf}`
-            );
-          }}
-          style={{
-            border: `1px solid gray`,
-          }}
-          className="mt-2 px-2 py-1 rounded-lg text-[12px]"
+    <>
+      {pkg?.image && (
+        <div
+          key={index}
+          className="bg-white shadow-md rounded-lg overflow-hidden"
         >
-          Download Handbills
-        </button>
-        <p className="mt-3 text-gray-600 text-[12px]">
-          <strong>Value Deal:</strong> {str}
-        </p>
+          {/* Fixed image container */}
+          <div className="w-full h-40 flex justify-center items-center bg-white-200">
+            <img
+              src={`data:image/png;base64,${pkg?.image}`}
+              alt={pkg.subject}
+              className="w-full h-full object-contain"
+            />
+          </div>
+          <div className="p-4">
+            <h3 className="text-sm font-bold">Here's Our {pkg.subject}</h3>
+            <button
+              onClick={() => {
+                ViewOrDownloandPDF(
+                  `/tnx_InvestigationAttchment/ViewDocument?Documentpath=${pkg?.pdf}`
+                );
+              }}
+              style={{
+                border: `1px solid gray`,
+              }}
+              className="mt-2 px-2 py-1 rounded-lg text-[12px]"
+            >
+              Download Handbills
+            </button>
+            <p className="mt-3 text-gray-600 text-[12px]">
+              <strong>Value Deal:</strong> {str}
+            </p>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+const SkeletonCard = () => {
+  return (
+    <div className="bg-gray-200 shadow-md rounded-lg overflow-hidden animate-pulse">
+      {/* Fixed image container */}
+      <div className="w-full h-40 bg-gray-300" />
+      <div className="p-4">
+        <div className="w-3/4 h-4 bg-gray-300 rounded mb-2" />
+        <div className="w-1/2 h-3 bg-gray-300 rounded mb-4" />
+        <div className="w-full h-3 bg-gray-300 rounded mb-2" />
+        <div className="w-2/3 h-3 bg-gray-300 rounded" />
       </div>
     </div>
   );
 };
 
+const UpdatesCarouselSkeleton = () => {
+  return (
+    <div className="relative w-full px-4">
+      {[...Array(3)].map((_, index) => (
+        <SkeletonCard key={index} />
+      ))}
+    </div>
+  );
+};
--- src/components/pages/adminctrlmaster/Dashboard/MarketingDashboard.jsx
+++ src/components/pages/adminctrlmaster/Dashboard/MarketingDashboard.jsx
@@ -1,43 +1,84 @@
 import React, { useEffect, useState } from "react";
-import { useGetData, usePostData } from "../../../../service/apiService";
+import { useGetData } from "../../../../service/apiService";
 import UpdatesCarousel from "./Components/UpdatesCarousel";
 import PackagesSpotlight from "./Components/PackagesSpotlight";
 import { StyledHr } from "../../../../Custom Components/DynamicTable";
 
 // Main Component
 export default function MarketingDashboard() {
-  // ------------------ Marketing -------------------------------
-  const [MarketingId, setMarketingId] = useState(null);
-  const [MarketingValue, setMarketingValue] = useState("New Updates");
-  const [MarketingDropDown, setMarketingDropDown] = useState(false);
-  const [MarketingHoveIndex, setMarketingHoveIndex] = useState(null);
-  const [MarketingSelectedOption, setMarketingSelectedOption] = useState("");
-
-  const [row, setRow] = useState([]);
+  const [NewUpdates, setNewUpdates] = useState([]);
+  const [LabOffer, setLabOffer] = useState([]);
+  const [PackageInSpotlight, setPackageInSpotlight] = useState([]);
+  const [RewardProgram, setRewardProgram] = useState([]);
+  const [MonthlySlab, setMonthlySlab] = useState([]);
   const getData = useGetData();
-  const spotLightData = useGetData();
-  const PostData = usePostData();
-  const DocumentData = usePostData();
-  const UpdateData = usePostData();
+
   useEffect(() => {
-    if (MarketingValue != "") {
-      getData?.fetchData(
-        `/MarketingDashBoard/GetDashBoardData?type=${MarketingValue}`
+    getAllData();
+  }, []); // Removed getData?.loading to avoid unnecessary re-renders
+
+  const getAllData = async () => {
+    try {
+      const resNewUpdates = await getData?.fetchData(
+        `/MarketingDashBoard/ViewMarketingDashboard?type=New Updates`
       );
+      if (resNewUpdates?.data?.success) {
+        setNewUpdates(resNewUpdates?.data?.data);
+      }
+      const resPackagesinSpotlight = await getData?.fetchData(
+        `/MarketingDashBoard/ViewMarketingDashboard?type=Packages in Spotlight`
+      );
+      if (resPackagesinSpotlight?.data?.success) {
+        setPackageInSpotlight(resPackagesinSpotlight?.data?.data);
+      }
+      const resLabOffer = await getData?.fetchData(
+        `/MarketingDashBoard/ViewMarketingDashboard?type=Lab Offer`
+      );
+      if (resLabOffer?.data?.success) {
+        setLabOffer(resLabOffer?.data?.data);
+      }
+      const resRewardProgram = await getData?.fetchData(
+        `/MarketingDashBoard/ViewMarketingDashboard?type=Reward Program`
+      );
+      if (resRewardProgram?.data?.success) {
+        setRewardProgram(resRewardProgram?.data?.data);
+      }
+      const resMonthlySlab = await getData?.fetchData(
+        `/MarketingDashBoard/ViewMarketingDashboard?type=Monthly Slab`
+      );
+      if (resMonthlySlab?.data?.success) {
+        setMonthlySlab(resMonthlySlab?.data?.data);
+      }
+    } catch (error) {
+      console.error("Error fetching marketing dashboard data:", error);
     }
-    spotLightData?.fetchData(
-      `/MarketingDashBoard/GetDashBoardData?type=Packages%20in%20Spotlight`
-    );
-  }, [MarketingValue, UpdateData?.loading, PostData?.loading]);
-  console.log(getData?.data?.data);
+  };
+
+  console.log(NewUpdates);
   return (
-    <>
-      <div>
-        <UpdatesCarousel updates={getData?.data?.data || []} />
-        <StyledHr style={{marginTop:"1px"}} />
-        <PackagesSpotlight packages={spotLightData?.data?.data || []} />
-        <StyledHr style={{marginTop:"30px"}} />
-      </div>
-    </>
+    <div>
+      <UpdatesCarousel updates={NewUpdates} />
+      <StyledHr style={{ marginTop: "1px" }} />
+      <PackagesSpotlight
+        packages={PackageInSpotlight}
+        title={"Package In Spotlight"}
+      />
+      <StyledHr style={{ marginTop: "1px" }} />
+       <PackagesSpotlight
+        packages={LabOffer}
+        title={"Lab Offer"}
+      />
+      <StyledHr style={{ marginTop: "1px" }} />
+       <PackagesSpotlight
+        packages={RewardProgram}
+        title={"Reward Program"}
+      />
+      <StyledHr style={{ marginTop: "1px" }} />
+       <PackagesSpotlight
+        packages={MonthlySlab}
+        title={"Monthly Slab"}
+      />
+      <StyledHr style={{ marginTop: "30px" }} />
+    </div>
   );
 }
